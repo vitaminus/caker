76396ed70c0a04c8a056144c175694af28c8035d
vitaminus <partadw@gmail.com>
2013-01-24 19:27:33 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Caker::Application.config.secret_token = 'b1757922b96c39290c0fab33dc21005935367c379dacccb3ee443ead89c010a37aeb63d3aec0c894bbda1016247a74bba9c5caaa4e6553121e66b904251e7619'
