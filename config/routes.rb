c93c7faee3b6d44e6b46e5fe395cc7ccc2a04187
vitaminus <partadw@gmail.com>
2013-01-25 11:48:52 +0200

    Add a full_title, delete index.html from public

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Caker::Application.routes.draw do
   get "static_pages/help"
   get "static_pages/about"
 
+  root :to => 'static_pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
