8041f88a9d7645f590a1208bf4b3dedd37bdfa3c
vitaminus <partadw@gmail.com>
2013-02-12 17:15:24 +0200

    Replace helpers into app.helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,6 @@
 # encoding: UTF-8
 module ApplicationHelper
 
-  include CountriesHelper
-  include CitiesHelper
-  include AreasHelper
-
   def full_title(page_title)
     base_title = "Торты на заказ"
     if page_title.empty?
@@ -12,6 +8,18 @@ module ApplicationHelper
     else
       "#{base_title} | #{page_title}"
     end
+  end
+
+  def country_find
+    @country = Country.find(params[:id])
+  end
+
+  def city_find
+    @city = City.find(params[:id])
+  end
+
+  def area_find
+    @area = Area.find(params[:id])
   end
 
 end
